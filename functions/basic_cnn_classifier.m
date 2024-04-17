6f28c5f1a02adbbad638579454ca75096f5ac03c
eliashed <eliashed@HB-110-02.studat.chalmers.se>
2024-04-17 15:19:55 +0200

    Ex 2.14 done

---
+++ functions/basic_cnn_classifier.m
@@ -0,0 +1,9 @@
+function layers = basic_cnn_classifier()
+layers = [
+    imageInputLayer([28 28 1]);
+    convolution2dLayer(3, 25);
+    reluLayer();
+    maxPooling2dLayer(2, Stride=2);
+    fullyConnectedLayer(10)
+    softmaxLayer()
+    classificationLayer()];
